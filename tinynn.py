910cc49cc28750c968b88adc578bdbad776e28bf
Alexey <alexeyqian@gmail.com>
2026-05-21 17:50:15 +0900

    add backward function

--- tinynn.py
+++ tinynn.py
@@ -3,9 +3,10 @@ import math
 import numpy as np
 
 class TinyNN():
-    def __init__(self, n_feature, n_classes):
+    def __init__(self, n_feature, n_classes, lr=0.01):
 	self.W = np.random.randn(n_features, n_classes)
 	self.b = np.zeros((n_features, 1))
+	self.lr = lr
 
     def softmax(self, scores_matrix):
 	exp_x = np.exp(scores_matrix)
@@ -22,4 +23,10 @@ class TinyNN():
 	loss = -np.sum(targets*np.log(preds+episilon))
 	return loss/preds.shape[0]
 
-
+    def backward(self, X, y, probs):
+	n_samples = X.shape[0]
+	d_logits = (probs - y)/n_samples
+	dW = X.T@d_logits
+	db = np.sum(d_logits, axis=0, keepdims=True)
+	self.W -= self.lr*dW
+	self.b -= self.lr*db
